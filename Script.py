1c89691b8ddf5c209f0b003121b5382a62b255ae
Xjdjsjs <a23568831@gmail.com>
2025-06-17 20:36:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {caption}
 
 ⚙️ sɪᴢᴇ : {file_size}
 
